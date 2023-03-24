af60130e029f79dde08ea324e45570196f7eafaf
Josh Icard <111021673+jicard@users.noreply.github.com>
2023-03-24 18:35:09 -0400

    removed get /

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-
+require("dotenv").config();
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
@@ -66,9 +66,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
